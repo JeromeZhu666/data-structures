867bb4b5435087633f46f62e766302b5f2ab2ac2
JeromeZhu666 <JeromeZhu666@gmail.com>
2018-10-25 23:14:39 +0800

    用数组实现循环队列

---
+++ src/xin/jerome/datastructures/queue/impl/MyLoopQueue.java
@@ -0,0 +1,148 @@
+package xin.jerome.datastructures.queue.impl;
+
+import xin.jerome.datastructures.queue.MyQueue;
+
+/**
+ * {@link MyQueue} 的循环队列实现
+ *
+ * @author Jerome Zhu
+ * @since 2018.10.25 21:55
+ */
+public class MyLoopQueue<E> implements MyQueue<E> {
+
+    /**
+     * 循环队列的容器
+     */
+    private E[] data;
+
+    /**
+     * 循环队列的队首的位置
+     */
+    private int front;
+
+    /**
+     * 循环队列的队尾的位置
+     */
+    private int tail;
+
+    /**
+     * 循环队列的大小
+     */
+    private int size;
+
+    /**
+     * 无参构造，使用默认大小
+     */
+    public MyLoopQueue() {
+        this(10);
+    }
+
+    /**
+     * 根据参数构造初始容量大小
+     *
+     * @param capacity 初始容量大小s
+     */
+    public MyLoopQueue(int capacity) {
+        data = (E[]) new Object[capacity + 1];
+        front = 0;
+        tail = 0;
+        size = 0;
+    }
+
+    @Override
+    public void enqueue(E e) {
+        // 如果循环队列满的话需要进行扩容
+        if ((tail + 1) % data.length == front)
+            resize(getCapacity() * 2);
+
+        data[tail] = e;
+        // 维护队尾的索引
+        tail = (tail + 1) % data.length;
+        // 维护队列大小
+        size++;
+    }
+
+    @Override
+    public E dequeue() {
+        // 判断队列是否为空，再进行出队操作
+        if (isEmpty())
+            throw new IllegalArgumentException("队列为空，不能进行出队操作！");
+        E e = data[front];
+        // 维护出队位置
+        data[front] = null;
+        // 维护队首的索引
+        front = (front + 1) % data.length;
+        // 维护队列大小
+        size--;
+        // 如果队列大小为容量的四分之一的话，进行缩容
+        if (size == getCapacity() / 4 && getCapacity() / 2 != 0)
+            resize(getCapacity() / 2);
+        return e;
+    }
+
+    @Override
+    public E getFront() {
+        // 判断队列是否为空，再进行出队操作
+        if (isEmpty())
+            throw new IllegalArgumentException("队列为空！");
+        return data[front];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return front == tail;
+    }
+
+    /**
+     * 获取队列的容量
+     *
+     * @return 容量的大小
+     */
+    public int getCapacity() {
+        return data.length - 1;
+    }
+
+    /**
+     * 对循环队列进行容量改变
+     *
+     * @param newCapacity 新的容量大小
+     */
+    private void resize(int newCapacity) {
+        E[] newData = (E[]) new Object[newCapacity + 1];
+        for (int i = 0; i < size; i++)
+            newData[i] = data[(i + front) % data.length];
+        data = newData;
+        front = 0;
+        tail = size;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("MyArrayQueue: front[");
+        for (int i = front; i != tail; i = (i + 1) % data.length) {
+            sb.append(data[i]);
+            if((i + 1) % data.length != tail)
+                sb.append(",");
+        }
+        sb.append("] tail");
+        sb.append(String.format("  size : %d , capacity : %d",size ,getCapacity()));
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        MyLoopQueue<Integer> queue = new MyLoopQueue <>(5);
+        for (int i = 0; i < 10; i++) {
+            queue.enqueue(i);
+            System.out.println(queue);
+            if (i % 3 == 0) {
+                Integer e = queue.dequeue();
+                System.out.println(String.format("Integer : %d dequeue .", e));
+            }
+        }
+    }
+}
---
+++ src/xin/jerome/datastructures/queue/impl/TestQueue.java
@@ -0,0 +1,44 @@
+package xin.jerome.datastructures.queue.impl;
+
+import xin.jerome.datastructures.queue.MyQueue;
+
+import java.util.Random;
+
+/**
+ * 测试队列与循环队列的性能
+ *
+ * @author Jerome Zhu
+ * @since 2018.10.25 22:52
+ */
+public class TestQueue {
+
+    public static Double testQueue(MyQueue<Integer> queue, int opCount) {
+        // 纳秒值
+        Long startTime = System.nanoTime();
+
+        Random random = new Random();
+        System.out.println("enqueue ...");
+        for (int i = 0; i < opCount; i++)
+            queue.enqueue(random.nextInt(Integer.MAX_VALUE));
+
+        System.out.println("dequeue ...");
+        for (int i = 0; i < opCount; i++) {
+            queue.dequeue();
+        }
+
+        Long endTime = System.nanoTime();
+        return (endTime - startTime) / 1000000000.0;
+    }
+
+    public static void main(String[] args) {
+        int opCount = 1000000;
+
+        MyArrayQueue<Integer> arrayQueue = new MyArrayQueue<>();
+        double arrayQueueTime = testQueue(arrayQueue, opCount);
+        System.out.println("MyArrayQueue, time:" + arrayQueueTime + "s");
+
+        MyLoopQueue<Integer> loopQueue = new MyLoopQueue<>();
+        double loopQueueTime = testQueue(loopQueue, opCount);
+        System.out.println("MyLoopQueue, time:" + loopQueueTime + "s");
+    }
+}
