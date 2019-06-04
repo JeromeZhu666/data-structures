05e61cfd1ab45bd49e0f918c2a2c8ab4cc0313d2
JeromeZhu <JeromeZhu666@gmail.com>
2019-06-04 15:13:19 +0800

    完成链表（带有尾节点）实现队列以及测试

--- src/xin/jerome/datastructures/queue/TestMyQueue.java
+++ src/xin/jerome/datastructures/queue/TestMyQueue.java
@@ -2,6 +2,7 @@ package xin.jerome.datastructures.queue;
 
 import xin.jerome.datastructures.queue.MyQueue;
 import xin.jerome.datastructures.queue.impl.MyArrayQueue;
+import xin.jerome.datastructures.queue.impl.MyLinkedListQueue;
 import xin.jerome.datastructures.queue.impl.MyLoopQueue;
 
 import java.util.Random;
@@ -17,19 +18,31 @@ public class TestMyQueue {
     public static void main(String[] args) {
         MyArrayQueue<Integer> arrayQueue = new MyArrayQueue<>();
         MyLoopQueue<Integer> loopQueue = new MyLoopQueue<>();
+        MyLinkedListQueue<Integer> linkedListQueue = new MyLinkedListQueue<>();
 //        testQueue(arrayQueue);
 //        testQueue(loopQueue);
+//        testQueue(linkedListQueue);
 
 
-        int opCount = 1000000;
+        int opCount = 100000;
 
         double arrayQueueTime = testQueue(arrayQueue, opCount);
         System.out.println("MyArrayQueue, time:" + arrayQueueTime + "s");
 
         double loopQueueTime = testQueue(loopQueue, opCount);
         System.out.println("MyLoopQueue, time:" + loopQueueTime + "s");
+
+        double linkedListQueueTime = testQueue(loopQueue, opCount);
+        System.out.println("MyLinkedListQueue, time:" + linkedListQueueTime + "s");
     }
 
+    /**
+     * 测试进行 opCount 个进队出队操作所需时间
+     *
+     * @param queue   {@link MyQueue}的实现类
+     * @param opCount 操作次数
+     * @return 花费的时间
+     */
     public static Double testQueue(MyQueue<Integer> queue, int opCount) {
         // 纳秒值
         Long startTime = System.nanoTime();
@@ -48,6 +61,9 @@ public class TestMyQueue {
         return (endTime - startTime) / 1000000000.0;
     }
 
+    /**
+     * 测试{@link MyQueue}功能
+     */
     private static void testQueue(MyQueue<Integer> queue) {
         for (int i = 0; i < 10; i++) {
             queue.enqueue(i);
